b668d3132794ff9a66c3f4f84a8371ad1c4fbb60
Michael Brown <mbrown@fensystems.co.uk>
2020-03-10 08:49:41 +0000

    npm: Add model for raw NPM package metadata
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

---
+++ pk/npm.py
@@ -0,0 +1,124 @@
+"""NPM packages
+
+The NPM package metadata format is documented at
+https://github.com/npm/registry/blob/master/docs/responses/package-metadata.md
+"""
+
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Callable
+from .base import (Attribute, DateTimeAttribute, DictAttribute, ListAttribute,
+                   Serializable, SerializableMapping, SerializableSequence)
+
+__all__ = [
+    'NpmPackage',
+]
+
+
+@dataclass
+class NpmHuman(Serializable):
+    """NPM human object"""
+
+    name = Attribute()
+    email = Attribute()
+    url = Attribute()
+
+
+@dataclass
+class NpmHumans(SerializableSequence):
+    """NPM human objects"""
+
+    type: Callable = NpmHuman
+
+
+@dataclass
+class NpmDist(Serializable):
+    """NPM dist object"""
+
+    tarball = Attribute()
+    shasum = Attribute()
+
+
+@dataclass
+class NpmRepository(Serializable):
+    """NPM repository object"""
+
+    type = Attribute()
+    url = Attribute()
+
+
+@dataclass
+class NpmTime(SerializableMapping):
+    """NPM publication times"""
+
+    type: Callable = DateTimeAttribute.type
+
+    created = DateTimeAttribute()
+    modified = DateTimeAttribute()
+
+
+@dataclass
+class NpmDistTags(SerializableMapping):
+    """NPM dist tags"""
+
+    latest = Attribute()
+
+
+@dataclass
+class NpmVersionHoisted(Serializable):
+    """NPM hoisted version information"""
+
+    author = Attribute(type=NpmHuman)
+    bugs = Attribute(type=NpmHuman)
+    contributors = ListAttribute(type=NpmHumans)
+    description = Attribute()
+    homepage = Attribute()
+    keywords = ListAttribute()
+    license = Attribute()
+    maintainers = ListAttribute(type=NpmHumans)
+    readme = Attribute()
+    readmeFilename = Attribute()
+    repository = Attribute(type=NpmRepository)
+
+
+@dataclass
+class NpmVersion(NpmVersionHoisted):
+    """NPM package version"""
+
+    name = Attribute()
+    version = Attribute()
+    deprecated = Attribute()
+    dependencies = DictAttribute()
+    optionalDependencies = DictAttribute()
+    devDependencies = DictAttribute()
+    bundleDependencies = DictAttribute()
+    peerDependencies = DictAttribute()
+    bin = DictAttribute()
+    directories = DictAttribute()
+    dist = Attribute(type=NpmDist)
+    engines = DictAttribute()
+    hasShrinkwap = Attribute('_hasShrinkwrap')
+    id = Attribute('_id')
+    nodeVersion = Attribute('_nodeVersion')
+    npmUser = Attribute('_npmUser', type=NpmHuman)
+    main = Attribute()
+
+
+@dataclass
+class NpmVersions(SerializableMapping):
+    """NPM package versions"""
+
+    type: Callable = NpmVersion
+
+
+@dataclass
+class NpmPackage(NpmVersionHoisted):
+    """An NPM package"""
+
+    id = Attribute('_id')
+    rev = Attribute('_rev')
+    dist_tags = Attribute('dist-tags', type=NpmDistTags)
+    name = Attribute()
+    time = Attribute(type=NpmTime)
+    users = DictAttribute()
+    versions = DictAttribute(type=NpmVersions)
---
+++ test/test_npm.py
@@ -0,0 +1,34 @@
+"""NPM tests"""
+
+from datetime import date
+from pathlib import Path
+import sys
+import unittest
+from pk.npm import NpmPackage
+
+
+class NpmPackageTest(unittest.TestCase):
+    """NPM package tests"""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.files = Path(sys.modules[cls.__module__].__file__).parent / 'files'
+
+    def test_json(self):
+        """Test JSON parsing"""
+        npm = NpmPackage(json=(self.files / 'leftpad.json').read_text())
+        self.assertEqual(npm.author.name, "Tom MacWright")
+        self.assertEqual(npm.dist_tags.latest, '0.0.1')
+        self.assertEqual(npm.dist_tags['latest'], '0.0.1')
+        self.assertEqual(npm.time.modified.date(), date(2018, 2, 27))
+        self.assertEqual(npm.time['0.0.1'].date(), date(2017, 5, 3))
+        self.assertFalse(npm.users)
+        self.assertEqual(npm.versions['0.0.0'].bugs.url,
+                         'https://github.com/tmcw/leftpad/issues')
+        self.assertIn('formatting', npm.versions['0.0.0'].keywords)
+        self.assertEqual(npm.versions['0.0.1'].maintainers[0].name, 'tmcw')
+        self.assertEqual(npm.versions['0.0.1'].repository.type, 'git')
+        self.assertEqual(npm.versions['0.0.1'].devDependencies['jsverify'],
+                         '^0.8.2')
+        self.assertEqual(npm.versions['0.0.1'].dist.shasum,
+                         '86b1a4de4face180ac545a83f1503523d8fed115')
